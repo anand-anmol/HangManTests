aa5f82b197ce96dea444c399670c523c5c86b93c
anmols2007 <46238519+anmols2007@users.noreply.github.com>
2019-12-01 22:46:58 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body><? echo("hello this is a test"); ?>
+<body>
+    <?
+    // Create connection
+    $servername = "den1.mysql4.gear.host";
+    $username = "hangmanwords";
+    $password = "Lw6XS?9Ts?36";
+    $conn = new mysqli($servername, $username, $password);
+    $name = '';
+    $points = 0;
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "INSERT INTO Leaderboard (name, points) VALUES ($name, $points)";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "You've been added to the leaderboard!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
     <script src="demo_newfile.js"></script>
 </body>
 </html>
